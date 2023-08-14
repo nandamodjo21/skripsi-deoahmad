4dcfb0145e5bd7b0c2d02d229ad6fb35ef88fe36
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-08-15 01:15:36 +0800

    feat: menambahkan model master biodata

---
+++ application/models/M_biodata.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_biodata extends CI_Model
+{
+
+    public function SemuaData(){
+        return $this->db->query("SELECT b.*, j.jenis_kelamin, a.agama FROM `t_biodata` b, t_jenis_kelamin j, t_agama a WHERE b.jk=j.id_jk AND b.id_agama=a.id_agama ORDER BY b.id_biodata ASC;");
+    }
+
+    public function getJk(){
+        return $this->db->query("SELECT * FROM t_jenis_kelamin");
+    }
+    public function getAgama(){
+        return $this->db->query("SELECT * FROM t_agama");
+    }
+    public function hapus_data($id_biodata)
+    {
+        $this->db->where('id_biodata', $id_biodata);
+        $this->db->delete('t_biodata');
+    }
+
+}
